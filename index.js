49b3325bfe637bb49ff40f1961f37fa80eac24e9
Patrick Roberts <patrob22@gmail.com>
2023-03-24 10:07:19 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+const superbowlWin = (array) => {
+    let sb = array.find(elem => elem.result === 'W');
+    
+    if (sb) {
+        return sb.year;
+    } else {
+        return undefined;
+    };
+};
